98d1bbdc4cf1df4f5f013c18ea5bb23806137fb0
Charlie Mackie <charliemackie@Charlies-MacBook-Pro.local>
2021-09-21 23:52:42 -0400

    without threading

--- rec_client.py
+++ rec_client.py
@@ -2,7 +2,6 @@ import cv2
 import pickle
 import struct
 import socket
-import threading
 
 client = socket.socket()
 
@@ -13,27 +12,24 @@ conn = client.connect((host, port))
 
 payload_size = struct.calcsize("Q")
 
-def rec():
-    while(True):
-        print("Connected @", conn)
-        try:
-            while len(data) < payload_size:
-                packet = conn.recv(4096)
-                if not packet: 
-                    break
-                data += packet
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack("Q", packed_msg_size)[0]
-            while len(data) < msg_size:
-                data += conn.recv(4096)
-            frame_data = data[:msg_size]
-            data  = data[msg_size:]
-            frame = pickle.loads(frame_data)
-            cv2.imshow("Receiving ...",frame)
-        except:
-            break
+while(True):
+    print("Connected @", conn)
+    try:
+        while len(data) < payload_size:
+            packet = conn.recv(4096)
+            if not packet: 
+                break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data  = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("Receiving ...",frame)
+    except:
+        break
 
-t1 = threading.Thread(target = rec)
-t1.start()
-t1.join()
+
--- send_client.py
+++ send_client.py
@@ -13,25 +13,21 @@ conn = client.connect((host, port))
 
 payload_size = struct.calcsize("Q")
 
-def send():
-    while(True):
-        print("Connected @", conn)
-        try:
-            # OpenCV function, this will open the webcam on current machine
-            vid = cv2.VideoCapture(0)
-            while(vid.isOpened()):
+while(True):
+    print("Connected @", conn)
+    try:
+        # OpenCV function, this will open the webcam on current machine
+        vid = cv2.VideoCapture(0)
+        while(vid.isOpened()):
 
-                # metadata from the video frames (ex: pixel RGB values)
-                img, frame = vid.read()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q", len(a)) + a
+            # metadata from the video frames (ex: pixel RGB values)
+            img, frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
 
-                # send frames from local webcam
-                conn.sendall(message)
-                cv2.imshow('Sending ...', frame)
-        except:
-            break
+            # send frames from local webcam
+            conn.sendall(message)
+            cv2.imshow('Sending ...', frame)
+    except:
+        break
 
-t1 = threading.Thread(target = send)
-t1.start()
-t1.join()
