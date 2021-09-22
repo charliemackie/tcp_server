53cff012f4e914357df0c6ade9efa2d02d1acd50
Charlie Mackie <charliemackie@Charlies-MacBook-Pro.local>
2021-09-22 00:04:30 -0400

    loop

--- rec_client.py
+++ rec_client.py
@@ -13,23 +13,21 @@ conn = client.connect((host, port))
 payload_size = struct.calcsize("Q")
 
 while(True):
-    print("Connected @", conn)
-    try:
-        while len(data) < payload_size:
-            packet = conn.recv(4096)
-            if not packet: 
-                break
-            data += packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
-        while len(data) < msg_size:
-            data += conn.recv(4096)
-        frame_data = data[:msg_size]
-        data  = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        cv2.imshow("Receiving ...",frame)
-    except:
-        break
+    while len(data) < payload_size:
+        packet = conn.recv(4096)
+        if not packet: 
+            break
+        data += packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Receiving ...",frame)
+    
+
 
 
--- send_client.py
+++ send_client.py
@@ -14,20 +14,17 @@ conn = client.connect((host, port))
 payload_size = struct.calcsize("Q")
 
 while(True):
-    print("Connected @", conn)
-    try:
-        # OpenCV function, this will open the webcam on current machine
-        vid = cv2.VideoCapture(-1)
-        while(vid.isOpened()):
+    # OpenCV function, this will open the webcam on current machine
+    vid = cv2.VideoCapture(0)
+    while(vid.isOpened()):
 
-            # metadata from the video frames (ex: pixel RGB values)
-            img, frame = vid.read()
-            a = pickle.dumps(frame)
-            message = struct.pack("Q", len(a)) + a
+        # metadata from the video frames (ex: pixel RGB values)
+        img, frame = vid.read()
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
 
-            # send frames from local webcam
-            conn.sendall(message)
-            cv2.imshow('Sending ...', frame)
-    except:
-        break
+        # send frames from local webcam
+        conn.sendall(message)
+        cv2.imshow('Sending ...', frame)
+    
 
