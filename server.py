3c8cb9eee112abdad5bc67fc705256a3825b41fb
charliemackie <51030219+charliemackie@users.noreply.github.com>
2021-10-27 19:02:00 -0400

    server

--- server.py
+++ server.py
@@ -13,19 +13,11 @@ def start():
     while(True):
         conn1, add1 = server.accept()
         conn2, add2 = server.accept()
-        conn3, add2 = server.accept()
-        conn4, add2 = server.accept()
         print(conn1)
         print(conn2)
-        print(conn3)
-        print(conn4)
-        while(conn1 != None and conn2 != None and conn3 != None and conn4 != None):
+        while(conn1 != None and conn2 != None):
             data = conn1.recv(4096)
             data = conn2.recv(4096)
-            data = conn3.recv(4096)
-            data = conn4.recv(4096)
             conn1.sendall(data)
             conn2.sendall(data)
-            conn3.sendall(data)
-            conn4.sendall(data)   
 start()
